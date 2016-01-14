ba1df6998b6f3d20b15ff31f292ac4c70ddae095
monikaavalur <monikaavalur@missionrnd.com>
2016-01-14 21:43:04 +0530

    Submit Commit

--- src/linkedListMedian.cpp
+++ src/linkedListMedian.cpp
@@ -21,16 +21,40 @@ struct node {
 
 int linkedListMedian(struct node *head) 
 {
-	struct node *previous_node = head;
-	struct node *current_node = head;
-	//traversing the linked list to find the median of the linked list
+	if (head == NULL )
+		return NULL;
+	if (head->next == NULL)
+		return head->num;
+	int length = 1;
+	struct node *ptr1 = head;  // Will move 1 node frwd
+	struct node *ptr2 = head;  // Will move 2 nodes frwd
 	if (head != NULL)
 	{
-		while (current_node != NULL&&current_node->next != NULL)
-		{
-			current_node = current_node->next->next;
-			previous_node = previous_node->next;
-		}
+		head = head->next;
+		length = length + 1;
+
 	}
-	return previous_node->num;
-}
+	printf("%d\n", length);
+	if (length % 2 != 0)
+	{
+		// we don't need to check for ptr1. because ptr2 ahead of ptr1
+		while (ptr2->next != NULL && ptr2->next->next != NULL)
+		{
+			ptr2 = ptr2->next->next;
+			ptr1 = ptr1->next;
+		}
+		return ptr1->num;
+	}
+	if (length % 2 == 0)
+	{
+		while (ptr2->next != NULL && ptr2->next->next != NULL)
+		{
+			ptr2 = ptr2->next->next;
+			ptr1 = ptr1->next;
+		}
+		int average = (ptr1->num + ptr1->next->num) / 2;
+		return average;
+	}
+
+
+	}
